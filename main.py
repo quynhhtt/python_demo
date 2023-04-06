36dcec4f4a9cf63c7d32ac3766c2082ea423a28e
MBF7-QUYNHHTT-01 <httquynh2011@gmail.com>
2023-04-06 22:34:59 +0700

    bai2

--- main.py
+++ main.py
@@ -11,24 +11,45 @@ def cal_income_tax(income: int, npt: int) -> float:
     tax = (income - fam_deduction - dependant_deduction * npt) * tax_interest
     return tax
 
-def cal_ielts_grade(reading:float, listening:float, speaking:float, writing:float) -> float:
-    grade = round((reading + listening + speaking + writing)/4, 1)
+
+def cal_ielts_grade(reading: float, listening: float, speaking: float, writing: float) -> float:
+    grade = round((reading + listening + speaking + writing) / 4, 1)
     return grade
 
-def play_keo_bua_bao(p1:str, p2: str) -> str:
+
+def play_keo_bua_bao(p1: str, p2: str):
     if p1 == p2:
         print('Draw')
     elif p1 == 'keo' and p2 == 'bao':
-            print('player 1 wins')
+        print('player 1 wins')
     elif p1 == 'bua' and p2 == 'keo':
-            print('player 1 wins')
+        print('player 1 wins')
     elif p1 == 'bao' and p2 == 'bua':
-            print('player 1 wins')
-    else: print('player 2 wins')
+        print('player 1 wins')
+    else:
+        print('player 2 wins')
 
-if __name__ == "__main__":
+
+def while_loop():
+    a = int(input('Enter a number:'))
+    while a < 100:
+        print(a)
+        a = int(input('Enter a number:'))
+
+
+def keobuabao_loop():
     p1 = str(input('Enter player 1''s choice: '))
     p2 = str(input('Enter player 2''s choice: '))
-    print(play_keo_bua_bao(p1=p1, p2=p2))
+    while (p1 == 'keo' and p2 == 'bao') or (p1 == 'bua' and p2 == 'keo') or (p1 == 'bao' and p2 == 'bua'):
+        print('player 1 wins')
+        p1 = str(input('Enter player 1''s choice: '))
+        p2 = str(input('Enter player 2''s choice: '))
 
+
+if __name__ == "__main__":
+    # p1 = str(input('Enter player 1''s choice: '))
+    # p2 = str(input('Enter player 2''s choice: '))
+    # play_keo_bua_bao(p1=p1, p2=p2)
+
+    keobuabao_loop()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
